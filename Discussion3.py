32b5f657ac47c46a326df5c7c449c1850683b8b8
Qinglan Luo <sherry@Qinglans-MacBook-Air.local>
2026-01-28 17:33:32 -0500

    fixed str

--- Discussion3.py
+++ Discussion3.py
@@ -12,7 +12,7 @@ class Rectangle():
 
     # Create the "__str__" method
     def __str__(self):
-        return str(self.width) + ", " + str(self.length)
+        return f"A rectangle with width {self.width} and height {self.length}"
     # YOUR CODE HERE
 
 
